18f16bb98aa0cd0a13052f4870ef77d6d579f047
Denys998 <denyscanchachavarria@gmail.com>
2025-05-02 01:16:43 -0500

    agregando script al trabajo

--- scripts/script_princpal.js
+++ scripts/script_princpal.js
@@ -1,4 +1,34 @@
 window.onload = function() {
     alert("Bienvenido a la página!");
   };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var botonesAgregar = document.querySelectorAll('.agregar-carrito');
+    var listaCarrito = document.getElementById('lista-carrito');
+    var totalElemento = document.getElementById('total');
+    var total = 0;
+    
+    botonesAgregar.forEach(function(boton) {
+        boton.addEventListener('click', function() {
+            var nombre = boton.getAttribute('data-nombre');
+            var precio = parseInt(boton.getAttribute('data-precio'));
+            var cantidadInput = boton.previousElementSibling;
+            var cantidad = parseInt(cantidadInput.value);
+            
+            if (cantidad > 0) {
+                var subtotal = precio * cantidad;
+
+                var item = document.createElement('li');
+                item.textContent = `${cantidad} x ${nombre} - s/${subtotal}`;
+                listaCarrito.appendChild(item);
+
+                total += subtotal;
+                totalElemento.textContent = total.toFixed(2);
+            } else {
+                alert("Por favor ingresa una cantidad válida");
+            }
+        });
+    });
+});
+
   
